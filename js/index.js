6162a997ef563938151a4711ea46e04a47f1d8d6
paing_soe_thaw <paingsoethaw@gmail.com>
2018-05-14 11:45:44 +0800

    init

---
+++ js/index.js
@@ -0,0 +1,35 @@
+// function Person(props) {
+//   return (
+//     <div className="person">
+//       <h1>{props.name}</h1>
+//       <p>Score: {props.score}</p>
+//     </div>
+//   );
+// }
+
+// var app = (
+//   <div>
+//     <Person name="David" score="12" />
+//     <Person name="John" score="15" />
+//   </div>
+// );
+
+// ReactDOM.render(app, document.querySelector("#app"));
+
+function Person(props) {
+  return React.createElement(
+    "div",
+    { className: "person" },
+    React.createElement("h1", null, props.name),
+    React.createElement("p", null, "Score: ", props.score)
+  );
+}
+
+var app = React.createElement(
+  "div",
+  null,
+  React.createElement(Person, { name: "David", score: "18" }),
+  React.createElement(Person, { name: "John", score: "15" })
+);
+
+ReactDOM.render(app, document.querySelector("#app"));
